a4d2eea149fb1dc9caeb8ef9ba0b411e991bcccf
Vardan Gevorgyan <vardanGevorgyan@student.unsw.edu.au>
2026-06-02 23:43:48 +1000

    downgrade mcp to 1.1.0

--- cloud_server.py
+++ cloud_server.py
@@ -8,7 +8,7 @@ from fastapi import FastAPI, WebSocket
 from mcp.server.fastmcp import FastMCP
 import uuid
 
-mcp = FastMCP("system health agent", auth=None)
+mcp = FastMCP("system health agent")
 app = FastAPI()
 
 relay_ws = None
